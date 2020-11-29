ee5b7764a3c5b4f8818fcdb0b84b0f0a0262cf27
Deeshan Sharma <deeshansharma04@gmail.com>
2020-11-29 09:50:34 +0000

    feat: Added some more questions

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var userName = read.question("Tell me your name: ");
 var welcomeMsg = "Welcome " + userName + " Let's see how much you know Deeshan Sharma.."
 console.log("\n" + welcomeMsg)
 
-console.log("\nThere will be some MCQ questions where you have to enter your choice which you feel is correct. And then you will get +1 score for every right answer. So, let's start\n")
+console.log("\nThere will be some MCQ questions where you have to enter your choice which you feel is correct. And then you will get +1 score for every right answer. So, let's start")
 
 score = 0;
 function question(que, options, corAns) {
-  console.log(que)
+  console.log("\n" + que)
   for(var i = 0; i < options.length; i++){
     console.log(`${i+1}. ${options[i]}`);
   }
@@ -17,9 +17,13 @@ function question(que, options, corAns) {
     score += 1;
     console.log(`You are right your current score: ${score}`)
   }
+  
   else{
-    console.log(`You are wrong your current score: ${score}`)
+    console.log(`You are wrong..! Correct Answer is ${options[corAns-1]}. Your current score: ${score}`)
   }
 }
 
-question("What is my age", [18, 20, 19, 21], 3)
+question("What is my age?", [18, 19, 20, 21], 2);
+question("What is my favorite movie type?", ["Action", "Love Story"], 1);
+question("What is my favorite fast-food?", ["Burger", "Momos", "Pizza", "Chowmine"], 3);
+question("Which country I dream to visit?", ["Russia", "Brazil", "Germany", "USA"], 4);
